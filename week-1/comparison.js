bfc45618f7d6518135a6b1af8fb8972a102175e2
AmishiR <145252272+AmishiR@users.noreply.github.com>
2024-10-01 12:32:29 +0000

    container config done

---
+++ week-1/comparison.js
@@ -0,0 +1,17 @@
+//console.log("2">1)///type changed to check
+
+    ////Null
+console.log(null>0)//0>0 is false
+console.log(null==0)// null here is kept as it is so false
+console.log(null>=0)//0>=0 so 0=0 so true
+
+    ////undefined
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined>=0)
+///all will be false same with NaN
+
+console.log("a" >= "3")//lexicographical (alphabetical) comparison of the two strings, character by character, based on their Unicode values.
+console.log('a'>=3)//here a into number will result to NaN
+//also word string will result to Nan ex="hello"
+
