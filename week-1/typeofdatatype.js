82f8d7b4c699fe2b6819c2ace357f349f6e6ab4e
AmishiR <145252272+AmishiR@users.noreply.github.com>
2024-10-01 09:36:24 +0000

    container config done

---
+++ week-1/typeofdatatype.js
@@ -0,0 +1,15 @@
+// let gender="female"
+// let name= "amishi"
+// if(gender =="female"){
+//     console.log("hello "+ name)
+// }
+console.log(typeof " ")
+console.log(typeof null)
+console.log(typeof undefined)//for variables that have not been defined
+console.log(typeof [ ])//
+console.log(typeof 12)//for both float and int its number datatype
+console.log(typeof true)
+console.log(typeof 10n)
+console.log(typeof Symbol())//creates a new symbol, which is a unique primitive data type in JavaScrip
+console.log(typeof Symbol);//because Symbol is a function that creates symbol values.
+console.log(typeof function(){})
