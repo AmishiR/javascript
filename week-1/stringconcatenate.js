beb2772ec9cd85367bff0b3ef017e44a854d2caf
AmishiR <145252272+AmishiR@users.noreply.github.com>
2024-10-01 11:02:16 +0000

    container config done

---
+++ week-1/stringconcatenate.js
@@ -0,0 +1,17 @@
+const str1="Hello"
+const str2="Amishi"
+
+/////TEMPLATE LITERAL
+const result1= `${str1} ${str2}`;
+// console.log(result1)
+
+/////Using + Operator
+// console.log(str1+ " "+str2)
+
+/////// Using concat()
+const result2= str1.concat(" ",str2)
+// console.log(result2)
+
+/////USING JOIN
+const result3= [str1 ,str2].join(" ");
+// console.log(result3)
